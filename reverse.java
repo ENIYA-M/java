c5eb3e55ef6d431f018af761ae76b617fc1073f6
ENIYA-M <nithya.mece@ksrce.ac.in>
2016-08-09 17:07:11 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,10 @@
+
+
+class InvertString
+{
+   public static void main(String args[])
+   {
+      StringBuffer b = new StringBuffer("Java programming ");
+      System.out.println(b.reverse());
+   }
+}
